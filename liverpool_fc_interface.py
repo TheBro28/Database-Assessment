50cba8c97e2b9fda1ba978ef89e15bf1bbac1922
TheBro28 <hamishwenham@riccarton.school.nz>
2025-06-24 08:39:07 +1200

    Added start of a parameter query for my positions table

--- liverpool_fc_interface.py
+++ liverpool_fc_interface.py
@@ -45,6 +45,48 @@ def print_query(view_name:str):
     print(tabulate(results,headings))
     db.close()
 
+# Import the libraries to connect to the database and present the information in tables
+
+import sqlite3
+
+from tabulate import tabulate
+
+
+# This is the filename of the database to be used
+
+DB_NAME = 'Liverpool_FC.db'
+
+# This is the SQL to connect to all the tables in the database
+
+TABLES = (" liverpool_fc "
+
+           "LEFT JOIN positions ON liverpool_fc.position_id = positions.position_id " )
+
+def print_parameter_query(fields:str, where:str, parameter):
+
+    """ Prints the results for a parameter query in tabular form. """
+
+    db = sqlite3.connect(DB_NAME)
+
+    cursor = db.cursor()
+
+    sql = ("SELECT " + fields + " FROM " + TABLES + " WHERE " + where)
+
+    cursor.execute(sql,(parameter,))
+
+    results = cursor.fetchall()
+
+    print(tabulate(results,fields.split(",")))
+
+    db.close()  
+
+
+
+
+position = input('What position players do you want to see: ')
+
+print_parameter_query("firstname, surname, appearances, awards, rating, clean_sheets, goals_conceded, wins, losses", "position = ? ORDER BY rating DESC",position)
+
 menu_choice =""
 while menu_choice != 'Z':
     menu_choice = input('Welcome to the Liverpool Football Club player statistics database\n\n'
